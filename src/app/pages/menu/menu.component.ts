ddc61bb66275f7a649e78d0ae384990ee0137123
nurulischweizer <nurul_i@dk-schweizer.com>
2024-03-15 10:05:21 +0800

    update menu permission

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CardModule } from 'primeng/card';
+import { RoleGuardDirective } from 'src/app/core/directives/role-guard.directive';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, CardModule, RouterModule],
+  imports: [CommonModule, RoleGuardDirective, CardModule, RouterModule],
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.less'],
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -14,6 +14,8 @@ import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
 import { PanelMenuModule } from 'primeng/panelmenu';
 import { SidebarModule } from 'primeng/sidebar';
+import { take } from 'rxjs';
+import { RoleService } from 'src/app/core/services/role.service';
 
 interface CustomMenuItem extends MenuItem {
   roles?: string[];
@@ -39,80 +41,79 @@ export class SidebarComponent {
   @Output() HideSideBarEmitter = new EventEmitter();
 
   userRoles: string[] = [];
-  items: MenuItem[] = [];
 
   private router = inject(Router);
+  readonly roleService = inject(RoleService);
 
-  ngOnInit(): void {
-    this.items = [
-      {
-        label: '<img src="assets/mdo.svg" width="7%"/>&nbsp; MDO',
-        escape: false,
-        visible: true,
-        command: () => {
-          this.router.navigateByUrl('/mdo/view');
-          this.HideSideBar();
+  items: CustomMenuItem[] = [
+    {
+      label: '<img src="assets/mdo.svg" width="7%"/>&nbsp; MDO',
+      escape: false,
+      visible: true,
+      command: () => {
+        this.router.navigateByUrl('/mdo/view');
+        this.HideSideBar();
+      },
+    },
+    {
+      label: '<img src="" width="7%"/>&nbsp; Settings',
+      escape: false,
+      visible: false,
+      roles: ['MDO.Admin'],
+      items: [
+        {
+          label: '\xa0\xa0\xa0\xa0\xa0\xa0' + 'Shipper',
+          command: () => {
+            this.router.navigateByUrl('/setting/shipper');
+            this.HideSideBar();
+          },
         },
-      },
-      {
-        label: '<img src="" width="7%"/>&nbsp; Settings',
-        escape: false,
-        visible: true,
-        items: [
-          {
-            label: '\xa0\xa0\xa0\xa0\xa0\xa0' + 'Shipper',
-            command: () => {
-              this.router.navigateByUrl('/setting/shipper');
-              this.HideSideBar();
-            },
+        {
+          label: '\xa0\xa0\xa0\xa0\xa0\xa0' + 'UOM',
+          command: () => {
+            this.router.navigateByUrl('/setting/uom');
+            this.HideSideBar();
           },
-          {
-            label: '\xa0\xa0\xa0\xa0\xa0\xa0' + 'UOM',
-            command: () => {
-              this.router.navigateByUrl('/setting/uom');
-              this.HideSideBar();
-            },
+        },
+        {
+          label: '\xa0\xa0\xa0\xa0\xa0\xa0' + 'Reason',
+          command: () => {
+            this.router.navigateByUrl('/setting/reason-code');
+            this.HideSideBar();
           },
-          {
-            label: '\xa0\xa0\xa0\xa0\xa0\xa0' + 'Reason',
-            command: () => {
-              this.router.navigateByUrl('/setting/reason-code');
-              this.HideSideBar();
-            },
+        },
+        {
+          label: '\xa0\xa0\xa0\xa0\xa0\xa0' + 'Ship To',
+          command: () => {
+            this.router.navigateByUrl('/setting/ship-to');
+            this.HideSideBar();
           },
-          {
-            label: '\xa0\xa0\xa0\xa0\xa0\xa0' + 'Ship To',
-            command: () => {
-              this.router.navigateByUrl('/setting/ship-to');
-              this.HideSideBar();
-            },
+        },
+        {
+          label: '\xa0\xa0\xa0\xa0\xa0\xa0' + 'Requester',
+          command: () => {
+            this.router.navigateByUrl('/setting/request-by');
+            this.HideSideBar();
           },
-          {
-            label: '\xa0\xa0\xa0\xa0\xa0\xa0' + 'Requester',
-            command: () => {
-              this.router.navigateByUrl('/setting/request-by');
-              this.HideSideBar();
-            },
+        },
+        {
+          label: '\xa0\xa0\xa0\xa0\xa0\xa0' + 'Manager User',
+          command: () => {
+            this.router.navigateByUrl('/user-manager/view');
+            this.HideSideBar();
           },
-          {
-            label: '\xa0\xa0\xa0\xa0\xa0\xa0' + 'Manager User',
-            command: () => {
-              this.router.navigateByUrl('/user-manager/view');
-              this.HideSideBar();
-            },
-          },
-        ],
-      },
-    ];
-  }
+        },
+      ],
+    },
+  ];
 
   constructor() {
-    // this.roleService.userRoleSubject.pipe(take(1)).subscribe((_roles) => {
-    //   this.userRoles = _roles;
-    //   this.SideMenuItems.forEach((menuItem) => {
-    //     if (menuItem.roles) menuItem.visible = this.IsVisible(menuItem.roles);
-    //   });
-    // });
+    this.roleService.userRoleSubject.pipe(take(1)).subscribe((_roles) => {
+      this.userRoles = _roles;
+      this.items.forEach((menuItem) => {
+        if (menuItem.roles) menuItem.visible = this.IsVisible(menuItem.roles);
+      });
+    });
   }
 
   IsVisible = (roles: string[]) =>
