3c1c5909bcbcf7f8531f77bb1bc0785fcc333766
yuenkai <yuenkai@outlook.com>
2024-04-03 17:45:11 +0800

    update to dk-schweizer.com

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   ApiBaseUrl: 'https://mdoapi.dk-schweizer.com',
-  RedirectUrl: 'https://mdoapp.dkcorp.com',
+  RedirectUrl: 'https://mdoapp.dk-schweizer.com',
   AuthServerUrl: 'https://authserver.dk-schweizer.com',
 };
