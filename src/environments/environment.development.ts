a5c789ad537457829dc3610ad5af4f7738f51574
yuenkai <yuenkai@outlook.com>
2024-11-04 10:48:07 +0800

    updatte

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,7 @@
 export const environment = {
   production: false,
-  ApiBaseUrl: 'https://localhost:7151',
+  //ApiBaseUrl: 'https://localhost:7151',
+  ApiBaseUrl: 'https://uatmdoapi.dk-schweizer.com',
   RedirectUrl: 'https://localhost:4200',
   AuthServerUrl: 'https://authserver.dk-schweizer.com',
-
-  // ApiBaseUrl: 'https://mdoapi.dk-schweizer.com',
-  // RedirectUrl: 'https://localhost:4200',
-  // AuthServerUrl: 'https://authserver.dk-schweizer.com',
 };
